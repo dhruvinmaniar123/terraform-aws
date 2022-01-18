a9cdaf40122e6c81f80f8d5771a9b6fd616b6ef5
dhruvinmaniar123 <maniardhruvin21@gmail.com>
2022-01-18 16:35:32 +0530

    adding ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+provider "azurerm" {  }
+#Configure AWS Instance
+
+resource "aws_instance" "my_ec2_demo" {
+  ami           = "ami-0851b76e8b1bce90b" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "My_interview_instance"
+  }
+}
